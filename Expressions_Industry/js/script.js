393444675a6ffe5cd1fec2f99f5d58cdd47df109
imgodzillason <jwcollins@fullsail.edu>
2014-11-05 18:15:03 -0500

    Adding additional variables
    Adding additional variables

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -14,10 +14,21 @@ var name = prompt("What is your name?");
 
 //ALERT user
 alert("Hello "+name+", we'll be asking you about your website needs.");
-//Ask user number of words required for website
+//Ask user number of words and images required for website
 //Create a variable to catch their answer
 
 var numberOfWords = prompt("How many words does your website require?");
+var numberOfImages = prompt("How many images does your website require?");
+
+//Ask user how many pages the website requires
+
+var numberOfPages = prompt("How many pages does your website require?");
 
 //Console.log out the response
 console.log(numberOfWords);
+console.log(numberOfImages);
+console.log(numberOfPages);
+
+//Number of words and divide by number of pages
+
+//
