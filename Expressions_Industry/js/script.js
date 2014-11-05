13e69024301928f6f346da6155456422dd4aad5d
imgodzillason <jwcollins@fullsail.edu>
2014-11-05 18:33:45 -0500

    Tidy up spacing
    Tidy up spacing

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -12,14 +12,13 @@ var name = prompt("What is your name?");
 
 //ALERT user
 alert("Hello "+name+", we'll be asking you about your website needs.");
+
 //Ask user number of words and images required for website
 //Create a variable to catch their answer
-
 var numberOfWords = prompt("How many words does your website require?");
 var numberOfImages = prompt("How many images does your website require?");
 
 //Ask user how many pages the website requires
-
 var numberOfPages = prompt("How many pages does your website require?");
 
 //Console.log out the response
@@ -28,13 +27,12 @@ console.log("Number of images input equals " +numberOfImages);
 console.log("Number of pages input equals " +numberOfPages);
 
 //Number of words and images divided by number of pages
-
 var wordsPerPage = numberOfWords / numberOfPages;
 var imagesPerPage = numberOfImages / numberOfPages;
 
 //ALERT user of number of words and images per page
-
 alert (name+ ", your website will require "+wordsPerPage+ " words per page and "+imagesPerPage+ " images per page.");
+
 //Console.log out the numbers for words and images
 console.log("Number of words per page equals " +wordsPerPage);
 console.log ("Number of images per page quals" +imagesPerPage);
