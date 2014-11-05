4b3ea02f55e9b616d6da6174dddb85fdbf34d354
imgodzillason <jwcollins@fullsail.edu>
2014-11-05 18:08:54 -0500

    Asked user for name and number of words
    Asked user for name and number of words needed for website

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -5,4 +5,19 @@ Expressions
 11/5/2014
  */
 
-alert ("test");
+/*alert ("test");*/
+
+//Create js file that will tell us number of words and images per web page on web site
+
+//Ask the user their name
+var name = prompt("What is your name?");
+
+//ALERT user
+alert("Hello "+name+", we'll be asking you about your website needs.");
+//Ask user number of words required for website
+//Create a variable to catch their answer
+
+var numberOfWords = prompt("How many words does your website require?");
+
+//Console.log out the response
+console.log(numberOfWords);
