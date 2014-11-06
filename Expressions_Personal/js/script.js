402f2b56ff13000c707f8420f91407ed2ee06236
imgodzillason <jwcollins@fullsail.edu>
2014-11-05 22:20:56 -0500

    Added alert for output of hours
    Added alert for output of hours the user would need to work

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -41,6 +41,10 @@ console.log(name+ " needs to work "+xboxOneHours+" hours to earn enough for an X
 console.log(name+ " needs to work "+playstationHours+" hours to earn enough for a Playstation 4.");
 console.log(name+ " needs to work "+wiiUHours+" hours to earn enough for a Wii U.");
 
+//ALERT user of number of hours
+alert(+name ", you will need to work" +xboxOneHours+" hours to earn enough for an Xbox One. You will need to work" +playstationHours+ " hours to earn enough for a Playstation 4. You will need to work" +wiiUHours+ " hours to earn enough for a Wii U.");
+
+
 //Ask user how many hours per day they work
 var hoursPerDay = prompt("How many hours per day do you work?");
 
