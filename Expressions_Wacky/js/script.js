d95491b1522028a88dde615d5f72d7992e6944a1
imgodzillason <jwcollins@fullsail.edu>
2014-11-05 23:41:38 -0500

    nameOfShow var added
    nameOfShow var added and prompt for user added

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -11,4 +11,7 @@
 var name = prompt("What is your name?");
 
 //console.log output for name
-console.log ("The user's name is"+name+".");
+console.log ("The user's name is "+name+".");
+
+//Ask the user what show they're watching
+var nameOfShow = prompt(name+", we're going to determine the amount of time you'll spend watching a series on Netflix. What show are you currently watching?");
