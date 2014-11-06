7cc61383db40fd69a841397694e0304e115f8211
imgodzillason <jwcollins@fullsail.edu>
2014-11-05 23:50:58 -0500

    Added lengthOfShow var
    Added lengthOfShow var and console.log output for it

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -27,3 +27,9 @@ var numberOfSeasons = prompt("How many seasons are there in "+nameOfShow+"?");
 
 //console.log for number of seasons
 console.log("There are "+numberOfSeasons+" seasons in "+nameOfShow+".");
+
+//Ask user how long each episode is
+var lengthOfShow = prompt("So, in minutes, about how long is each episode"+name+"?");
+
+//console.log output for length of show
+console.log ("Each episode of " +nameOfShow+ " is"+lengthOfShow+" minutes long")
