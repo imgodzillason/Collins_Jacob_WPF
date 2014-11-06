681739330b1aa8cde0ca6fbd9827143dbcd863a9
imgodzillason <jwcollins@fullsail.edu>
2014-11-06 01:26:51 -0500

    Begin Expressions Worksheet
    Begin Expressions Worksheet

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,16 @@
+/*
+ Jacob Collins
+ WPF Section 01
+ Expressions Worksheet
+ 11/5/2014
+ */
+
+//alert("test");
+
+//Dog Years
+
+//Ask user how old Sparky is
+var sparkyAge = prompt("How old is Sparky?");
+
+//console.log output for Sparky's age in human years
+console.log("Sparky is "+sparkyAge+" in human years.");
