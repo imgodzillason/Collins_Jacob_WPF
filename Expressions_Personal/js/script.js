df4edbf09e829ca8380597fc200c70118c1d7d0d
imgodzillason <jwcollins@fullsail.edu>
2014-11-05 21:07:36 -0500

    create first alert
    create first alert

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -5,4 +5,10 @@
  11/5/2014
  */
 
-alert("test");
+/*alert("test");*/
+
+//Ask user their name
+var name = prompt("What is your name?");
+
+//ALERT user
+alert("Hello "+name+", we're going to determine the number of hours you will have to work to purchase different gaming consoles.");
