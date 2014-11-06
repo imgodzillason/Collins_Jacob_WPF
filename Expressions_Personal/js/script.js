e84516e029fd533cc8a355d1e2a6615e67d41b6f
imgodzillason <jwcollins@fullsail.edu>
2014-11-05 22:39:34 -0500

    fixed spacing issues
    fixed spacing issues

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -63,7 +63,7 @@ console.log (name+ "needs to work " +numberOfDaysWiiU+ " days to earn a Wii U.")
 
 //ALERT user for number of days
 
-alert(name+", you will need to work "+numberOfDaysXbox+" days to earn an Xbox One. You will need to work" +numberOfDaysPlaystation+ " days to earn a Playstation 4. You will need to work" +numberOfDaysWiiU+" days to earn a Wii U.");
+alert(name+", you will need to work "+numberOfDaysXbox+" days to earn an Xbox One. You will need to work " +numberOfDaysPlaystation+ " days to earn a Playstation 4. You will need to work " +numberOfDaysWiiU+" days to earn a Wii U.");
 
 //Ask user how amount of coupon
 
