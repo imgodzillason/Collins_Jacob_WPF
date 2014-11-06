2ca55756c7c16bafdfff127411adebe6a13e20b4
imgodzillason <jwcollins@fullsail.edu>
2014-11-06 00:43:45 -0500

    Tidying up code
    Tidying up code and removing spaces

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -5,8 +5,6 @@
  11/5/2014
  */
 
-//alert ("test")
-
 //Ask user their name
 var name = prompt("What is your name?");
 
@@ -41,8 +39,8 @@ var numberOfEpisodes = prompt("How many episodes are there per season, "+name+"?
 console.log ("There are "+numberOfEpisodes+" episodes per season of " +nameOfShow+".");
 
 //Determine minutes and hours of show available
-var minutesOfShow = (lengthOfShow * numberOfEpisodes) * numberOfSeasons;
-var hoursOfShow = ((lengthOfShow * numberOfEpisodes) * numberOfSeasons) / 60;
+var minutesOfShow = (lengthOfShow * numberOfEpisodes) * numberOfSeasons; //multiplying length of show by number of episodes and then multiplying that by the number of seasons
+var hoursOfShow = ((lengthOfShow * numberOfEpisodes) * numberOfSeasons) / 60; //multiplying length of show by the number of episodes and then by the number of seasons. After this, dividing the number by 60 to get the number of hours
 
 //console.log output for minutes and hours of show
 console.log ("There are "+minutesOfShow+ " minutes of "+nameOfShow+" available to watch.");
@@ -52,7 +50,7 @@ console.log ("There are "+hoursOfShow+ " hours of " +nameOfShow+ " available to 
 alert(name+", there are "+minutesOfShow+ " minutes, or "+hoursOfShow+" hours, of "+nameOfShow+". What are you waiting for? Get started!");
 
 //hummingbird randomness
-var hummingbirdFlap = (60 * lengthOfShow) * 70;
+var hummingbirdFlap = (60 * lengthOfShow) * 70; //determine how many times hummingbird flapped its wings during each episode input by user
 
-//ALERT user of random hummingbird fact
+//ALERT user of random hummingbird fact and number of times it flapped its wings while they watched an episode of their show
 alert("Before you go, " +name+"! Did you know a hummingbird flaps its wings up to 70 times a second!? That means by the time you've finished an episode, it will have potentially flapped its wings "+hummingbirdFlap+ " times! Isn't nature awesome!? Now go. Go sit and do nothing while you watch " +nameOfShow+". Don't feel guilty. You aren't a hummingbird!");
