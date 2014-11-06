cd55ed67498b624715739e9e0e670ed572fe1160
imgodzillason <jwcollins@fullsail.edu>
2014-11-05 23:30:01 -0500

    Ask user name
    Ask user name and name var added

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -5,4 +5,10 @@
  11/5/2014
  */
 
-alert ("test")
+//alert ("test")
+
+//Ask user their name
+var name = prompt("What is your name?");
+
+//console.log output for name
+console.log ("The user's name is"+name+".");
