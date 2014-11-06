dfdf859a596061ee6581fd6bff5387093b96ef2a
imgodzillason <jwcollins@fullsail.edu>
2014-11-05 21:59:45 -0500

    fixed spacing issue in console.log
    fixed spacing issue in console.log

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -40,10 +40,10 @@ var wiiUHours = gamingConsoles [2] / payPerHour;
 var daysWorked = prompt("How many days a week do you work?");
 
 //console.log output for days per week worked
-console.log(name+ " works" +daysWorked+ " days per week.");
+console.log(name+ " works " +daysWorked+ " days per week.");
 
 //Ask user how many hours per day they work
 var hoursPerDay = prompt("How many hours per day do you work?");
 
 //console.log for hours per day worked
-console.log(name+ " works" +hoursPerDay+ " hours per day.");
+console.log(name+ " works " +hoursPerDay+ " hours per day.");
