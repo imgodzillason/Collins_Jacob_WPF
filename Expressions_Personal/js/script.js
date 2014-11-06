808147d54ae14163b679893b5703665cd988c2e2
imgodzillason <jwcollins@fullsail.edu>
2014-11-05 22:38:50 -0500

    Added alert for user
    Added alert for user defining number of days

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -61,6 +61,10 @@ console.log(name+ " needs to work " +numberOfDaysXbox+ " days to earn an Xbox On
 console.log(name+ " needs to work " +numberOfDaysPlaystation+ " days to earn a Playstation 4.");
 console.log (name+ "needs to work " +numberOfDaysWiiU+ " days to earn a Wii U.");
 
+//ALERT user for number of days
+
+alert(name+", you will need to work "+numberOfDaysXbox+" days to earn an Xbox One. You will need to work" +numberOfDaysPlaystation+ " days to earn a Playstation 4. You will need to work" +numberOfDaysWiiU+" days to earn a Wii U.");
+
 //Ask user how amount of coupon
 
 var couponAmount = prompt("How much does your coupon reduce the cost of the console? If you do not have one, enter 0.");
