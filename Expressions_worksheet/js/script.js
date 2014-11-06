138744f4f335d5b2c31d3d9f1347678a19453225
imgodzillason <jwcollins@fullsail.edu>
2014-11-06 01:39:24 -0500

    Slice of Pie Part 1
    Slice of Pie Part 1

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -12,5 +12,26 @@
 //Ask user how old Sparky is
 var sparkyAge = prompt("How old is Sparky?");
 
-//console.log output for Sparky's age in human years
-console.log("Sparky is "+sparkyAge+" in human years.");
+//Sparky's age multiplied by 7, or dog years
+var sparkyDogYears = sparkyAge * 7
+
+//print output for Sparky's age in dog years
+console.log("Sparky is " +sparkyAge+" human years old, which is "+sparkyDogYears+" in dog years.");
+
+//Slice of Pie part 1
+
+//Slices of pizza input by user
+var slicesPerPizza = prompt("How many slices of pizza are there per pizza?");
+
+//Number of people at party input by user
+var peopleAtParty = prompt("How many people are at the party?");
+
+//Number of pizzas ordered input by user
+var pizzasOrdered = prompt ("How many pizzas were ordered?");
+
+//Number of slices per person
+//Multiply slices of pizza by the number of pizzas ordered and divide it by the number of people at the party
+var slicesPerPerson = (slicesPerPizza * pizzasOrdered) / peopleAtParty;
+
+//print output for number of slices each person gets
+console.log("Each person ate "+slicesPerPerson+" slices of pizza at the party.");
