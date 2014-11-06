671739a34a7e7fb8f2b3bf366588bd60190fcde3
imgodzillason <jwcollins@fullsail.edu>
2014-11-05 23:46:06 -0500

    numberOfSeasons var added
    numberOfSeasons var added

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -18,3 +18,12 @@ var nameOfShow = prompt(name+", we're going to determine the amount of time you'
 
 //console.log output for name of show
 console.log(name+ " is currently watching "+nameOfShow+".");
+
+//ALERT user of choice
+alert ("You're watching "+nameOfShow+", which is a most excellent choice!");
+
+//Ask user how many seasons there are
+var numberOfSeasons = prompt("How many seasons are there in "+nameOfShow+"?");
+
+//console.log for number of seasons
+console.log("There are "+numberOfSeasons+" seasons in "+nameOfShow+".");
