29a1b67b1dea0c38e61cc434618ec54bf231e934
imgodzillason <jwcollins@fullsail.edu>
2014-11-11 22:36:07 -0500

    Following along with screencast
    Following along with screencast

--- Screencast-Conditionals/js/script.js
+++ Screencast-Conditionals/js/script.js
@@ -17,7 +17,7 @@ if (kidHeight > minHeight){
     console.log("Sorry kid, you've got some growing to do first!");
 }
 */
-
+/*
 var budget = 100;
 var iPhonePrice = 199.99;
 var wonLottery = false;
@@ -28,3 +28,28 @@ if(iPhonePrice < budget ||wonLottery === true){
 }else{
     console.log("No phone for you!!");
 }
+    */
+var gpa = 48;
+
+//if the gpa is over the min 2.0 score the student can graduate
+/*if(gpa > 2.0){
+    console.log("You can graduate!");
+}else{
+    console.log("GPA is too low!");
+}*/
+
+/*(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA is too low!");*/
+
+var age = 6;
+var book;
+
+//If the child is under 1-, they get Green Eggs and Ham, otherwise they get The Time Machine
+/*if(age <10){
+    book = "Green Eggs and Ham";
+}else{
+    book = "The Time Machine";
+}
+console.log(book);*/
+
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book);
