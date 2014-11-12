11750855eeffa57768924c0bfa2d44b2a8f19879
imgodzillason <jwcollins@fullsail.edu>
2014-11-11 22:26:44 -0500

    Following along with screencast
    Following along with screencast

--- Screencast-Conditionals/js/script.js
+++ Screencast-Conditionals/js/script.js
@@ -1,5 +1,5 @@
 //Conditional Logic
-
+/*
 var kidHeight = 46;
 var minHeight = 48;
 var wParentHeight = 45; //the height of the kid with parent
@@ -15,4 +15,16 @@ if (kidHeight > minHeight){
 } else{
     //code performed if condition is false
     console.log("Sorry kid, you've got some growing to do first!");
+}
+*/
+
+var budget = 100;
+var iPhonePrice = 199.99;
+var wonLottery = false;
+
+//if price of phone is less than our budget and if our paycheck is over 300
+if(iPhonePrice < budget ||wonLottery === true){
+    console.log("We can buy the phone!");
+}else{
+    console.log("No phone for you!!");
 }
