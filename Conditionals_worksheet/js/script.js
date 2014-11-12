a332e4c142e9c44122d178d8c8119162509587f9
imgodzillason <jwcollins@fullsail.edu>
2014-11-12 16:41:30 -0500

    Temp worksheet tested and completed
    Temp worksheet tested and completed

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -5,3 +5,33 @@ Activity: Conditionals Worksheet
 11/11/2014
  */
 
+//set up var conversion for Celsius and Fahrenheit
+var celsius = (fahrenheit - 32) / (5/9);
+var fahrenheit = (celsius - 32) * (5/9);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//obtain user info about temperature
+var temp = prompt("What is the temperature outside?");
+
+//obtain user info about Celsius or Fahrenheit
+var tempMeasure = prompt ("Do you want it converted to Celsius or Fahrenheit? Enter C for Celsius and F for Fahrenheit.");
+
+//convert measurements for temp.
+if(tempMeasure === "C"){
+    console.log("The temperature is " +((temp - 32) * (5/9))+ " degrees Celsius");
+}
+if(tempMeasure === "F"){
+    console.log("The temperature is "+((temp * (9/5)) +32)+ " degrees Fahrenheit");
+}
