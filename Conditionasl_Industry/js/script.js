a80f9df034c1a9bef27521cd1da25f4d2729a6a2
imgodzillason <jwcollins@fullsail.edu>
2014-11-13 17:20:21 -0500

    Working with if/else statements
    Working with if/else statements

--- Conditionasl_Industry/js/script.js
+++ Conditionasl_Industry/js/script.js
@@ -6,18 +6,20 @@
  */
 
 //obtain user info on number of hours needed to complete site
-var siteHoursNeeded = prompt("How many hours do you need to complete the client's website?");
-if(siteHoursNeeded = ""){
+var hoursNeeded = prompt("How many hours do you need to complete the client's website?");
+//validate number was entered
+if(hoursNeeded == ""){
     alert("You didn't enter any numbers.");
     prompt("How many hours do you need to complete the client's website?");
 }else{
     //print to console
-    console.log("The client's site requires" +siteHoursNeeded+ " hours to complete.")
+    console.log("The client's site requires " +hoursNeeded+ " hours to complete.")
 }
 
 //obtain number of employees working on site
 var numberOfEmployees = prompt("How many employees do you have working on the site?");
-if(numberOfEmployees = ""){
+//validate number was entered
+if(numberOfEmployees == ""){
     alert("You didn't enter any numbers.");
     prompt("How many employees do you have working on the site?");
 }else{
@@ -25,3 +27,17 @@ if(numberOfEmployees = ""){
     console.log("You have " +numberOfEmployees+ " employees working on the client's site.");
 }
 
+//number of hours per person
+var hoursPerEmployee = (+hoursNeeded) / (+numberOfEmployees);
+
+//print to console
+console.log("Each employee needs to work " +hoursPerEmployee+ " hours to complete the project in a week.");
+
+//determine if we need to hire a temp to complete site
+if (hoursPerEmployee >= 40 && hoursNeeded >= 500){
+    //print to console
+    console.log("Each employee will work "+hoursPerEmployee+ " this week. You should hire a temp to complete this on time.")
+}else{
+    //print to console
+    console.log("Each employee will work " +hoursPerEmployee+ " this week. You do not need to hire a temp.")
+}
