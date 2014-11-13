d749eead62ea8e7775daae721d71b4b1052c0f26
imgodzillason <jwcollins@fullsail.edu>
2014-11-13 00:32:07 -0500

    Set variables for Conditionals_Personal
    Set variables for Conditionals_Personal

---
+++ Conditionals_Personal/js/script.js
@@ -0,0 +1,12 @@
+/*
+Jacob Collins
+WPF Section 01
+Conditionals Personal
+11/12/14
+ */
+
+//obtain values for caloric intake in breakfast, lunch, and dinner
+var breakfast = prompt("How many calories did you have for breakfast?");
+var lunch = prompt("How many calories did you have for lunch?");
+var dinner = prompt("How many calories did you have for dinner?");
+
