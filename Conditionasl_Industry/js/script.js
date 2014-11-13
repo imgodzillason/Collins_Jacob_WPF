3cb3d6cc992135d1d83b00ad2b67e44e6923da8b
imgodzillason <jwcollins@fullsail.edu>
2014-11-13 17:25:54 -0500

    Enter comments
    Enter comments

--- Conditionasl_Industry/js/script.js
+++ Conditionasl_Industry/js/script.js
@@ -9,6 +9,7 @@
 var hoursNeeded = prompt("How many hours do you need to complete the client's website?");
 //validate number was entered
 if(hoursNeeded == ""){
+    //alert user no number was entered and prompt for number again
     alert("You didn't enter any numbers.");
     prompt("How many hours do you need to complete the client's website?");
 }else{
@@ -20,6 +21,7 @@ if(hoursNeeded == ""){
 var numberOfEmployees = prompt("How many employees do you have working on the site?");
 //validate number was entered
 if(numberOfEmployees == ""){
+    //alert user no number was entered and prompt for number again
     alert("You didn't enter any numbers.");
     prompt("How many employees do you have working on the site?");
 }else{
@@ -33,11 +35,11 @@ var hoursPerEmployee = Math.round((+hoursNeeded) / (+numberOfEmployees));
 //print to console
 console.log("Each employee needs to work " +hoursPerEmployee+ " hours to complete the project in a week.");
 
-//determine if we need to hire a temp to complete site
+//determine if we need to hire a temp to complete site on time
 if (hoursPerEmployee >= 40 || hoursNeeded >= 1000){
-    //print to console
+    //print to console if need to hire temp
     console.log("Each employee will work " +hoursPerEmployee+ " hours this week. You should consider hiring a temp to complete this on time.")
 }else{
-    //print to console
+    //print to console if don't need to hire temp
     console.log("Each employee will work " +hoursPerEmployee+ " hours this week. You do not need to hire a temp.")
 }
