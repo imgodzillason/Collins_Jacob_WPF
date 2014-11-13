eb0ac1c84d4154d44b1635637c6ab723994f73e8
imgodzillason <jwcollins@fullsail.edu>
2014-11-13 15:33:51 -0500

    Added if/else for Conditionals_personal
    Added if/else for Conditionals_personal

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -5,8 +5,38 @@ Conditionals Personal
 11/12/14
  */
 
-//obtain values for caloric intake in breakfast, lunch, and dinner
+//obtain values for caloric intake for breakfast
 var breakfast = prompt("How many calories did you have for breakfast?");
+
+//make sure values aren't empty
+if(breakfast == ""){
+    alert("You forgot to input a number.");
+    prompt("How many calories did you have for breakfast?");
+}else{
+    //print to console
+    console.log("You had "+breakfast+ " calories at breakfast.")
+    alert("We will now ask for your lunch calories.")
+}
+
+//obtain values for caloric intake for lunch
 var lunch = prompt("How many calories did you have for lunch?");
+
+//make sure values aren't empty
+if(lunch == ""){
+    alert("You forgot to input a number.");
+    prompt("How many calories did you have for lunch?")
+}else{
+    //print to console
+    console.log("You had "+lunch+ " calories at lunch.")
+    alert("We will now ask for your dinner calories.");
+}
+
 var dinner = prompt("How many calories did you have for dinner?");
+if (dinner =""){
+    alert("You forgot to input a number.");
+    prompt("How many calories did you have for dinner.");
+}else{
+    //print to console
+    console.log("You had "+dinner+ " calories at dinner.");
+}
 
