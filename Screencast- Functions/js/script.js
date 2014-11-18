c3fe4870dc86a78f7e9d0e41a84bb5bd98a2695e
imgodzillason <jwcollins@fullsail.edu>
2014-11-18 18:44:47 -0500

    Following along with screencast
    Following along with screencast

--- Screencast- Functions/js/script.js
+++ Screencast- Functions/js/script.js
@@ -3,3 +3,14 @@ Jacob Collins
 Screencast- Functions
 11/18/14
  */
+
+function outptMsg(){
+    console.log("Hello World");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
