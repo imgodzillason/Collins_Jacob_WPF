209ce1974ee87584ebd4f351534b69c974ca184d
imgodzillason <jwcollins@fullsail.edu>
2014-11-19 01:16:08 -0500

    Complete Loops screencast
    Complete Loops screencast

---
+++ Screencast - Loops/js/script.js
@@ -0,0 +1,34 @@
+/*
+Jacob Collins
+WPF- Section 1
+Screencast Loops
+11/19/14
+ */
+
+//While loops
+var b = 10; //sets up index
+
+while (b > 0){ //checks condition
+    console.log(b + " kegs on the wall." );
+    b--;//increments (++) or decrements (--) the index
+}
+
+
+
+var c = 10;
+
+// Do/while loops
+do{
+    console.log(c + " kegs on the wall.");
+    c--;
+}
+while (c > 0);
+
+
+
+//For loops
+
+for (var i = 10; i > 0; i--){
+    console.log(i + " kegs on the wall.");
+}
+
