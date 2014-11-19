c829ffcbb23e158dbc07d5ff9c610307012931a5
imgodzillason <jwcollins@fullsail.edu>
2014-11-19 00:51:38 -0500

    Completed functions worksheet
    Completed functions worksheet

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -8,21 +8,21 @@ Functions Worksheet
 //Calculate the circumference of a circle
 
 function calcCircumference(){
-    var radius = prompt("What is the radius of the circle?");
-    finalCircumference = 2 * 3.14 * radius;
-    console.log("The circumference of the circle is "+finalCircumference+ ".");
-    return finalCircumference;
+    var radius = prompt("What is the radius of the circle?");//request radius of circle
+    finalCircumference = 2 * 3.14 * radius; //circumerence of circle equals 2 times pi (3.14) times radius
+    console.log("The circumference of the circle is "+finalCircumference+ "."); //print result
+    return finalCircumference;//return value
 
 }
-calcCircumference();
+calcCircumference(); //call function for use
 
 //Calculate the number of bee stings to kill an animal
 
 function beeStings(){
-    var victimWeight = prompt("What is the weight of the animal stung?");
-    stingKills = 8.666666667 * victimWeight;
-    console.log("It takes "+stingKills+ " bee stings to kill this animal.");
-    return stingKills;
+    var victimWeight = prompt("What is the weight of the animal stung?"); //request weight of animal
+    stingKills = 8.666666667 * victimWeight;//multiply number of bee stings by weight given by user
+    console.log("It takes "+stingKills+ " bee stings to kill this animal.");//print result
+    return stingKills;//return value
 }
 
-beeStings();
+beeStings();//call function for use
