2d99f6efdf336af3d2686e6e7f62a5616ecadf67
imgodzillason <jwcollins@fullsail.edu>
2014-11-20 16:18:51 -0500

    Setting up variables
    Setting up variables for Functions_Personal

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -5,3 +5,24 @@ WPF- Section 1
 Functions_Personal
  */
 
+//set up variable requesting square feet
+var squareFeet = prompt("How many square feet is the room?");
+while((isNaN(squareFeet) || squareFeet === "")){
+    squareFeet = prompt("Please enter a number.");
+}
+
+console.log("The room is "+squareFeet+ " square feet.");
+
+var amount = prompt("How much is the tile you want to purchase per square foot?");
+while((isNaN(amount) || amount ==="")){
+    amount = prompt("Please enter a number");
+}
+
+console.log("The amount you entered for your tile is $" +amount+ ".");
+
+var budget = prompt("How much have you budgeted to spend on tile?");
+while ((isNaN(budget) || budget ==="")){
+    budget = prompt("Please enter an amount.");
+}
+
+console.log("You have allotted $" +budget+ " to spend on tile.");
