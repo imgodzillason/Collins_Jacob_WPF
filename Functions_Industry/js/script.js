a87f0c1c8de944998f5258ab5d2e54959e5de084
imgodzillason <jwcollins@fullsail.edu>
2014-11-20 18:03:28 -0500

    Adding variables
    Adding variables to Functions_Industry

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -3,3 +3,8 @@ WPF- Section 1
 11/20/14
 Functions_Industry
 */
+
+var pixelWidth = prompt("What is the width (in pixels) that your website will be contained within?");
+while(isNaN(pixelwidth) || pixelWidth ===""){
+    pixelWidth = prompt("Please enter a number");
+}
