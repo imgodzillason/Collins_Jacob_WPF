6cdf7a0dce5bece7c7b9c8bd4ae0303f64040acb
imgodzillason <jwcollins@fullsail.edu>
2014-11-20 21:35:15 -0500

    Adding variables for planets
    Adding variables for planets and if statements

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -14,3 +14,29 @@ console.log("Your age is " +age+ ".");
 
 var ageInDays = age * 365;
 console.log("You are " +ageInDays+ " days old.");
+
+function planetaryAge(){
+    var choosePlanet = prompt("Select a planet and find out your age on that planet!");
+    while(choosePlanet === ""){
+        choosePlanet = prompt("Please enter a planet name.");
+    }
+    if (choosePlanet === "Mercury"){
+        var mercury = Math.round(ageInDays / 88);
+        console.log("You are approximately " +mercury+ " years old on Mercury.");
+    }
+   if(choosePlanet === "Venus"){
+       var venus = Math.round(ageInDays / 224.7);
+       console.log("You are approximately " +venus+ " years old on Venus");
+   }
+    if (choosePlanet === "Earth"){
+        console.log("You're the same age as you are now!");
+    }
+    if (choosePlanet === "Mars"){
+        var mars = Math.round( ageInDays /687);
+        console.log("You are approximately " +mars+ " years old on Mars.");
+    }
+    if (choosePlanet === "Jupiter"){
+        var jupiter = Math.round(ageInDays / 4332);
+        console.log("You are approximately " +jupiter+ " years old on Jupiter.");
+    }
+}
