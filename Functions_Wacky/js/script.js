3baad4235a6232952493a8066c45bc6385f87d33
imgodzillason <jwcollins@fullsail.edu>
2014-11-20 21:58:45 -0500

    Add comments
    Add comments to Functions_Wacky (completed)

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -20,40 +20,40 @@ function planetaryAge(){//create function to find user age on other planets
     while(choosePlanet === ""){//verify user entered information
         choosePlanet = prompt("Please enter a planet name.");//if user did not, prompt to enter information
     }
-    if (choosePlanet === "Mercury" || choosePlanet === "mercury"){
+    if (choosePlanet === "Mercury" || choosePlanet === "mercury"){//do this if user selects mercury
         var mercury = Math.round(ageInDays / 88);
         console.log("You are approximately " +mercury+ " years old on Mercury.");
     }
-   if(choosePlanet === "Venus" || choosePlanet === "venus"){
+   if(choosePlanet === "Venus" || choosePlanet === "venus"){//do this if user selects venus
        var venus = Math.round(ageInDays / 224.7);
        console.log("You are approximately " +venus+ " years old on Venus");
    }
-    if (choosePlanet === "Earth" || choosePlanet === "earth"){
+    if (choosePlanet === "Earth" || choosePlanet === "earth"){//do this if user selects earth
         console.log("You're the same age as you are now!");
     }
-    if (choosePlanet === "Mars" || choosePlanet === "mars"){
+    if (choosePlanet === "Mars" || choosePlanet === "mars"){//do this if user selects mars
         var mars = Math.round( ageInDays /687);
         console.log("You are approximately " +mars+ " years old on Mars.");
     }
-    if (choosePlanet === "Jupiter" || choosePlanet === "jupiter"){
+    if (choosePlanet === "Jupiter" || choosePlanet === "jupiter"){//do this if user selects jupiter
         var jupiter = Math.round(ageInDays / 4332);
         console.log("You are approximately " +jupiter+ " years old on Jupiter.");
     }
-    if (choosePlanet === "Saturn" || choosePlanet === "saturn"){
+    if (choosePlanet === "Saturn" || choosePlanet === "saturn"){//do this if user selects saturn
         var saturn = Math.round(ageInDays / 10760);
         console.log("You are approximately " +saturn+ " years old on Saturn.");
     }
-    if (choosePlanet === "Uranus" || choosePlanet === "uranus"){
+    if (choosePlanet === "Uranus" || choosePlanet === "uranus"){//do this if user selects uranus
         var uranus = ageInDays / 30700;
         console.log("You are approximately " +uranus+ " years old on Uranus.");
     }
-    if (choosePlanet === "Neptune" || choosePlanet === "neptune"){
+    if (choosePlanet === "Neptune" || choosePlanet === "neptune"){//do this if user selects neptune
         var neptune = ageInDays / 60200;
         console.log("You are approximately " +neptune+ " years old on Neptune.");
-    }if (choosePlanet === "Pluto" || choosePlanet === "pluto"){
+    }if (choosePlanet === "Pluto" || choosePlanet === "pluto"){//do this if user selects pluto
         var pluto = ageInDays / 90600;
         console.log("I'm glad you still consider it a planet! You'd be approximately " +pluto+ " years old on Pluto.");
 }
 }
 
-planetaryAge();
+planetaryAge();//call function to execute
